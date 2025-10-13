3062cdd672981ff190eaa7681910bf03d7eb97e2
1goshan1 <burdinskiygv322@gmail.com>
2025-10-13 22:18:25 +0400

    oops

--- lab2/src/main/java/ru/ssau/tk/cheefkeef/laba2/io/FunctionsIO.java
+++ lab2/src/main/java/ru/ssau/tk/cheefkeef/laba2/io/FunctionsIO.java
@@ -112,9 +112,4 @@ public final class FunctionsIO {
         objectOutputStream.flush();
     }
 
-    public static TabulatedFunction deserialize(BufferedInputStream stream) throws IOException, ClassNotFoundException {
-        ObjectInputStream objectInputStream = new ObjectInputStream(stream);
-        return (TabulatedFunction) objectInputStream.readObject();
-    }
-
 }
