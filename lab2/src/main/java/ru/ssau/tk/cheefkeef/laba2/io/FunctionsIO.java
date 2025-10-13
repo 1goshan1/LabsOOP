a1bc331b6f9b5657ecd38c6c65a6b92e10567db5
Alex Flitcher <76988822+alexflitcher1@users.noreply.github.com>
2025-10-13 19:03:16 +0400

    FunctionsIO

---
+++ lab2/src/main/java/ru/ssau/tk/cheefkeef/laba2/io/FunctionsIO.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.cheefkeef.laba2.io;
+
+public final class FunctionsIO {
+
+    private FunctionsIO() {
+        throw new UnsupportedOperationException("Utility class FunctionsIO cannot be instantiated");
+    }
+}
