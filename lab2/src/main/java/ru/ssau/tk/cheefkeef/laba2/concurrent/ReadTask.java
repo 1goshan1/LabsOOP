c02294823c8a88b7548f68f26abbc4f53020e865
1goshan1 <burdinskiygv322@gmail.com>
2025-10-19 16:45:28 +0400

    added concurrent package and ReadTask class

---
+++ lab2/src/main/java/ru/ssau/tk/cheefkeef/laba2/concurrent/ReadTask.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.cheefkeef.laba2.concurrent;
+
+
+import ru.ssau.tk.cheefkeef.laba2.functions.TabulatedFunction;
+
+public class ReadTask implements Runnable {
+    private final TabulatedFunction function;
+
+    public ReadTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        int count = function.getCount();
+        for (int i = 0; i < function.getCount(); ++i) {
+                double x = function.getX(i);
+                double y = function.getY(i);
+                System.out.printf("After read: i = %d, x = %f, y = %f%n", i, x, y); // %d = i, %f = x, next %f = y, %n = \n no diff
+        }
+    }
+}
