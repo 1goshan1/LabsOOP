4ef385476e678adce0a563844fa5e1251a550387
1goshan1 <burdinskiygv322@gmail.com>
2025-10-19 20:33:33 +0400

    added ReadWriteTaskExecutor(works not good, but it is going to be fixed in 2 lines of code)

---
+++ lab2/src/main/java/ru/ssau/tk/cheefkeef/laba2/concurrent/ReadWriteTaskExecutor.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.cheefkeef.laba2.concurrent;
+
+import ru.ssau.tk.cheefkeef.laba2.functions.ConstantFunction;
+import ru.ssau.tk.cheefkeef.laba2.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.cheefkeef.laba2.functions.TabulatedFunction;
+
+public class ReadWriteTaskExecutor {
+
+    public static void main(String[] args) {
+        ConstantFunction constantNegative = new ConstantFunction(-1); // -1 тк в задании как пример указано, ну а зачем думать, когда можно не думать
+        TabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(constantNegative, 1.0, 1000.0, 1000);
+
+        ReadTask readTask = new ReadTask(tabulatedFunction);
+        WriteTask writeTask = new WriteTask(tabulatedFunction, 0.5); // аналогичное число из задания
+
+        Thread readerThread = new Thread(readTask);
+        Thread writerThread = new Thread(writeTask);
+
+        readerThread.start();
+        writerThread.start();
+    }
+}
