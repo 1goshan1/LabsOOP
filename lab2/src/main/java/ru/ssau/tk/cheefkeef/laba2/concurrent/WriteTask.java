585a5339e005ebb9b38835c4aa8b23a434aea9ab
1goshan1 <burdinskiygv322@gmail.com>
2025-10-19 16:45:28 +0400

    added concurrent package and ReadTask class

---
+++ lab2/src/main/java/ru/ssau/tk/cheefkeef/laba2/concurrent/WriteTask.java
@@ -0,0 +1,21 @@
+package ru.ssau.tk.cheefkeef.laba2.concurrent;
+
+import ru.ssau.tk.cheefkeef.laba2.functions.TabulatedFunction;
+
+public class WriteTask implements Runnable {
+    private final TabulatedFunction function;
+    double value;
+    public WriteTask(TabulatedFunction function, double value) {
+        this.function = function;
+        this.value = value;
+    }
+
+    @Override
+    public void run() {
+        int count = function.getCount();
+        for (int  i = 0; i < count; ++i) {
+            function.setY(i ,value);
+            System.out.printf("Writing for index %d complete%n", i);
+        }
+    }
+}
