0b6e319902579e1a9d9ecccb3c3c8f76a3da0a36
Alex Flitcher <76988822+alexflitcher1@users.noreply.github.com>
2025-10-20 18:22:46 +0400

    MultiplyingTask

---
+++ lab2/src/main/java/ru/ssau/tk/cheefkeef/laba2/concurrent/MultiplyingTask.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.cheefkeef.laba2.concurrent;
+
+import ru.ssau.tk.cheefkeef.laba2.functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable {
+
+    private final TabulatedFunction func;
+
+    public MultiplyingTask(TabulatedFunction func) {
+        this.func = func;
+    }
+
+    @Override
+    public void run() {
+        int count = func.getCount();
+        for (int i = 0; i < count; i++) {
+            double y = func.getY(i);
+            func.setY(i, y * 2);
+        }
+        System.out.println("Поток " + Thread.currentThread().getName() + " завершил выполнение задачи.");
+    }
+}
